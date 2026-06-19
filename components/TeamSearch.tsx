d2077617032a298c0b23e955eb3f8bf5dbe17971
Lee <lee_astley@hotmail.co.uk>
2026-06-19 21:37:38 +0100

    fix syntax error in team search

--- components/TeamSearch.tsx
+++ components/TeamSearch.tsx
@@ -4,40 +4,41 @@ import { useState, useEffect, useRef } from "react";
 import { Team } from "@/lib/types";
 
 const UK_TEAMS: Team[] = [
-  { id: 57,  name: "Arsenal FC",              shortName: "Arsenal",      tla: "ARS", crest: "https://crests.football-data.org/57.png",  competition: "Premier League" },
-  { id: 328, name: "Burnley FC",              shortName: "Burnley",      tla: "BUR", crest: "https://crests.football-data.org/328.png", competition: "Premier League" },
-  { id: 61,  name: "Chelsea FC",              shortName: "Chelsea",      tla: "CHE", crest: "https://crests.football-data.org/61.png",  competition: "Premier League" },
-  { id: 354, name: "Crystal Palace FC",       shortName: "Crystal Palace",tla: "CRY", crest: "https://crests.football-data.org/354.png", competition: "Premier League" },
-  { id: 62,  name: "Everton FC",              shortName: "Everton",      tla: "EVE", crest: "https://crests.football-data.org/62.png",  competition: "Premier League" },
-  { id: 63,  name: "Fulham FC",               shortName: "Fulham",       tla: "FUL", crest: "https://crests.football-data.org/63.png",  competition: "Premier League" },
-  { id: 64,  name: "Liverpool FC",            shortName: "Liverpool",    tla: "LIV", crest: "https://crests.football-data.org/64.png",  competition: "Premier League" },
-  { id: 65,  name: "Manchester City FC",      shortName: "Man City",     tla: "MCI", crest: "https://crests.football-data.org/65.png",  competition: "Premier League" },
-  { id: 66,  name: "Manchester United FC",    shortName: "Man United",   tla: "MUN", crest: "https://crests.football-data.org/66.png",  competition: "Premier League" },
-  { id: 67,  name: "Newcastle United FC",     shortName: "Newcastle",    tla: "NEW", crest: "https://crests.football-data.org/67.png",  competition: "Premier League" },
-  { id: 68,  name: "Nottingham Forest FC",    shortName: "Nott'm Forest",tla: "NOT", crest: "https://crests.football-data.org/68.png",  competition: "Premier League" },
-  { id: 73,  name: "Tottenham Hotspur FC",    shortName: "Spurs",        tla: "TOT", crest: "https://crests.football-data.org/73.png",  competition: "Premier League" },
-  { id: 74,  name: "West Ham United FC",      shortName: "West Ham",     tla: "WHU", crest: "https://crests.football-data.org/74.png",  competition: "Premier League" },
-  { id: 76,  name: "Wolverhampton Wanderers FC", shortName: "Wolves",   tla: "WOL", crest: "https://crests.football-data.org/76.png",  competition: "Premier League" },
-  { id: 397, name: "Brentford FC",            shortName: "Brentford",    tla: "BRE", crest: "https://crests.football-data.org/397.png", competition: "Premier League" },
-  { id: 402, name: "Brighton & Hove Albion FC", shortName: "Brighton",  tla: "BHA", crest: "https://crests.football-data.org/402.png", competition: "Premier League" },
-  { id: 340, name: "Southampton FC",          shortName: "Southampton",  tla: "SOU", crest: "https://crests.football-data.org/340.png", competition: "Premier League" },
-  { id: 356, name: "AFC Bournemouth",         shortName: "Bournemouth",  tla: "BOU", crest: "https://crests.football-data.org/356.png", competition: "Premier League" },
-  { id: 563, name: "Aston Villa FC",          shortName: "Aston Villa",  tla: "AVL", crest: "https://crests.football-data.org/563.png", competition: "Premier League" },
-  { id: 383, name: "Leicester City FC",       shortName: "Leicester",    tla: "LEI", crest: "https://crests.football-data.org/383.png", competition: "Championship" },
-  { id: 387, name: "Leeds United FC",         shortName: "Leeds",        tla: "LEE", crest: "https://crests.football-data.org/387.png", competition: "Championship" },
-  { id: 384, name: "Middlesbrough FC",        shortName: "Middlesbrough",tla: "MID", crest: "https://crests.football-data.org/384.png", competition: "Championship" },
-  { id: 389, name: "Swansea City AFC",        shortName: "Swansea",      tla: "SWA", crest: "https://crests.football-data.org/389.png", competition: "Championship" },
-  { id: 390, name: "Cardiff City FC",         shortName: "Cardiff",      tla: "CAR", crest: "https://crests.football-data.org/390.png", competition: "Championship" },
-  { id: 394, name: "Queens Park Rangers FC",  shortName: "QPR",          tla: "QPR", crest: "https://crests.football-data.org/394.png", competition: "Championship" },
-  { id: 408, name: "Ipswich Town FC",         shortName: "Ipswich",      tla: "IPS", crest: "https://crests.football-data.org/408.png", competition: "Championship" },
-  { id: 427, name: "Sunderland AFC",          shortName: "Sunderland",   tla: "SUN", crest: "https://crests.football-data.org/427.png", competition: "Championship" },
-  { id: 715, name: "Stoke City FC",           shortName: "Stoke",        tla: "STK", crest: "https://crests.football-data.org/715.png", competition: "Championship" },
-  { id: 750, name: "Sheffield United FC",     shortName: "Sheffield Utd",tla: "SHU", crest: "https://crests.football-data.org/750.png", competition: "Championship" },
-  { id: 351, name: "Coventry City FC",        shortName: "Coventry",     tla: "COV", crest: "https://crests.football-data.org/351.png", competition: "Championship" },
-  { id: 714, name: "Portsmouth FC",           shortName: "Portsmouth",   tla: "POR", crest: "https://crests.football-data.org/714.png", competition: "Championship" },
-  { id: 380, name: "Derby County FC",         shortName: "Derby",        tla: "DER", crest: "https://crests.football-data.org/380.png", competition: "Championship" },
-  { id: 9825, name: "Celtic FC",              shortName: "Celtic",       tla: "CEL", crest: "https://crests.football-data.org/9825.png",competition: "Scottish Premiership" },
-  { id: 9826, name: "Rangers FC",             shortName: "Rangers",      tla: "RAN", crest: "https://crests.football-data.org/9826.png",competition: "Scottish Premiership" },
+  { id: 57,  name: "Arsenal FC",                 shortName: "Arsenal",       tla: "ARS", crest: "https://crests.football-data.org/57.png",  competition: "Premier League" },
+  { id: 61,  name: "Chelsea FC",                 shortName: "Chelsea",       tla: "CHE", crest: "https://crests.football-data.org/61.png",  competition: "Premier League" },
+  { id: 62,  name: "Everton FC",                 shortName: "Everton",       tla: "EVE", crest: "https://crests.football-data.org/62.png",  competition: "Premier League" },
+  { id: 63,  name: "Fulham FC",                  shortName: "Fulham",        tla: "FUL", crest: "https://crests.football-data.org/63.png",  competition: "Premier League" },
+  { id: 64,  name: "Liverpool FC",               shortName: "Liverpool",     tla: "LIV", crest: "https://crests.football-data.org/64.png",  competition: "Premier League" },
+  { id: 65,  name: "Manchester City FC",         shortName: "Man City",      tla: "MCI", crest: "https://crests.football-data.org/65.png",  competition: "Premier League" },
+  { id: 66,  name: "Manchester United FC",       shortName: "Man United",    tla: "MUN", crest: "https://crests.football-data.org/66.png",  competition: "Premier League" },
+  { id: 67,  name: "Newcastle United FC",        shortName: "Newcastle",     tla: "NEW", crest: "https://crests.football-data.org/67.png",  competition: "Premier League" },
+  { id: 68,  name: "Nottingham Forest FC",       shortName: "Nott Forest",   tla: "NOT", crest: "https://crests.football-data.org/68.png",  competition: "Premier League" },
+  { id: 73,  name: "Tottenham Hotspur FC",       shortName: "Spurs",         tla: "TOT", crest: "https://crests.football-data.org/73.png",  competition: "Premier League" },
+  { id: 74,  name: "West Ham United FC",         shortName: "West Ham",      tla: "WHU", crest: "https://crests.football-data.org/74.png",  competition: "Premier League" },
+  { id: 76,  name: "Wolverhampton Wanderers FC", shortName: "Wolves",        tla: "WOL", crest: "https://crests.football-data.org/76.png",  competition: "Premier League" },
+  { id: 328, name: "Burnley FC",                 shortName: "Burnley",       tla: "BUR", crest: "https://crests.football-data.org/328.png", competition: "Premier League" },
+  { id: 338, name: "Leicester City FC",          shortName: "Leicester",     tla: "LEI", crest: "https://crests.football-data.org/338.png", competition: "Premier League" },
+  { id: 340, name: "Southampton FC",             shortName: "Southampton",   tla: "SOU", crest: "https://crests.football-data.org/340.png", competition: "Premier League" },
+  { id: 354, name: "Crystal Palace FC",          shortName: "Crystal Palace",tla: "CRY", crest: "https://crests.football-data.org/354.png", competition: "Premier League" },
+  { id: 356, name: "AFC Bournemouth",            shortName: "Bournemouth",   tla: "BOU", crest: "https://crests.football-data.org/356.png", competition: "Premier League" },
+  { id: 397, name: "Brentford FC",               shortName: "Brentford",     tla: "BRE", crest: "https://crests.football-data.org/397.png", competition: "Premier League" },
+  { id: 402, name: "Brighton and Hove Albion FC",shortName: "Brighton",      tla: "BHA", crest: "https://crests.football-data.org/402.png", competition: "Premier League" },
+  { id: 563, name: "Aston Villa FC",             shortName: "Aston Villa",   tla: "AVL", crest: "https://crests.football-data.org/563.png", competition: "Premier League" },
+  { id: 387, name: "Leeds United FC",            shortName: "Leeds",         tla: "LEE", crest: "https://crests.football-data.org/387.png", competition: "Championship" },
+  { id: 383, name: "Luton Town FC",              shortName: "Luton",         tla: "LUT", crest: "https://crests.football-data.org/383.png", competition: "Championship" },
+  { id: 384, name: "Middlesbrough FC",           shortName: "Middlesbrough", tla: "MID", crest: "https://crests.football-data.org/384.png", competition: "Championship" },
+  { id: 389, name: "Swansea City AFC",           shortName: "Swansea",       tla: "SWA", crest: "https://crests.football-data.org/389.png", competition: "Championship" },
+  { id: 390, name: "Cardiff City FC",            shortName: "Cardiff",       tla: "CAR", crest: "https://crests.football-data.org/390.png", competition: "Championship" },
+  { id: 394, name: "Queens Park Rangers FC",     shortName: "QPR",           tla: "QPR", crest: "https://crests.football-data.org/394.png", competition: "Championship" },
+  { id: 408, name: "Ipswich Town FC",            shortName: "Ipswich",       tla: "IPS", crest: "https://crests.football-data.org/408.png", competition: "Championship" },
+  { id: 427, name: "Sunderland AFC",             shortName: "Sunderland",    tla: "SUN", crest: "https://crests.football-data.org/427.png", competition: "Championship" },
+  { id: 715, name: "Stoke City FC",              shortName: "Stoke",         tla: "STK", crest: "https://crests.football-data.org/715.png", competition: "Championship" },
+  { id: 750, name: "Sheffield United FC",        shortName: "Sheffield Utd", tla: "SHU", crest: "https://crests.football-data.org/750.png", competition: "Championship" },
+  { id: 351, name: "Coventry City FC",           shortName: "Coventry",      tla: "COV", crest: "https://crests.football-data.org/351.png", competition: "Championship" },
+  { id: 714, name: "Portsmouth FC",              shortName: "Portsmouth",    tla: "POR", crest: "https://crests.football-data.org/714.png", competition: "Championship" },
+  { id: 380, name: "Derby County FC",            shortName: "Derby",         tla: "DER", crest: "https://crests.football-data.org/380.png", competition: "Championship" },
+  { id: 9825, name: "Celtic FC",                 shortName: "Celtic",        tla: "CEL", crest: "https://crests.football-data.org/9825.png",competition: "Scottish Premiership" },
+  { id: 9826, name: "Rangers FC",                shortName: "Rangers",       tla: "RAN", crest: "https://crests.football-data.org/9826.png",competition: "Scottish Premiership" },
 ];
 
 interface Props {
@@ -81,7 +82,7 @@ export default function TeamSearch({ onSelect, selected }: Props) {
           if (selected) onSelect(null as unknown as Team);
         }}
         onFocus={() => setOpen(true)}
-        placeholder="e.g. Arsenal, Leeds United…"
+        placeholder="e.g. Arsenal, Leeds United"
         style={{
           width: "100%", background: "#11111a",
           border: "1px solid #2a2a3a", borderRadius: 8,
@@ -110,11 +111,15 @@ export default function TeamSearch({ onSelect, selected }: Props) {
               onMouseEnter={(e) => (e.currentTarget.style.background = "#1e1e2e")}
               onMouseLeave={(e) => (e.currentTarget.style.background = "transparent")}
             >
-              {t.crest && (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img src={t.crest} alt="" width={22} height={22} style={{ objectFit: "contain" }} />
-              )}
+              <img src={t.crest} alt="" width={22} height={22} style={{ objectFit: "contain" }} />
               <div>
                 <div style={{ fontSize: 14, fontWeight: 600 }}>{t.name}</div>
-                {t.competition && (
-                  <div style={{ fontSize: 11, color: "#6060a0" }}>{t.competition}</div>
+                <div style={{ fontSize: 11, color: "#6060a0" }}>{t.competition}</div>
+              </div>
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
